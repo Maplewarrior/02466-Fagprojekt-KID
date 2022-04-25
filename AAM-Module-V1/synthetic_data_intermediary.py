832375d5e56a38290928bfeaf220fb8e7e60a12f
HarbX <michaelharborg@gmail.com>
2022-04-25 11:00:59 +0200

    Intermediary script for synthetic

--- AAM-Module-V1/synthetic_data_intermediary.py
+++ AAM-Module-V1/synthetic_data_intermediary.py
@@ -3,14 +3,18 @@ from scipy.special import softmax
 from scipy.stats import norm
 
 
-def betaConstraints(betas):
+def betaConstraintsNoBias(betas):
     
-    betas = betas / sum(betas)
     new_betas = np.empty(len(betas))
+    denom = sum(betas)
     
-    for i in range(len(betas)):
-        new_betas[i] = sum(betas[:i])
-   
+    for i in range(len(new_betas)):
+        new_betas[i] = np.sum(betas[:i]) / denom
+    
+    # betas = np.concatenate((np.array([betas[0]]), betas))
+    # betas = softmax(betas)
+    # new_betas = np.cumsum(betas, axis = 0)[:len(betas)-1]
+    
     return new_betas
 
 
@@ -19,42 +23,41 @@ def softPlus(sigma):
 
 def get_Z(M, K, p):
     # Assure reproducibility
-    np.random.seed(42)
+    np.random.seed(123)
     
     # betas = np.arange(1,p)
-    betas = np.ones(p-1)*5
-    # Calculate beta-values
-    betas = betaConstraints(betas)
-    # betas = softmax(betas)
+    # betas = np.ones(p-1)*5
+    betas = np.arange(1,p)
+    betas = betaConstraintsNoBias(betas)
     
+    betas = np.array([0.00, 0.4, 0.8, 1.2, 1.6])
     
     alphas = np.empty(len(betas)+1)
-    Z = np.empty((M, K))
-    
-   
     
     # Calculate alpha-values
-    for i in range(len(betas)):
-        if not i == (len(betas)-1) or 0:
-            alphas[i] = (betas[i]+betas[i+1])/2
-        elif i == 0:
-            alphas[i] = (0+betas[i])/2
-        elif i == (len(betas)-1):
-            alphas[i] = (betas[i]+1)/2
+    alphas[0] = (0 + betas[0]) / 2
+    alphas[-1] = (1+ betas[-1]) / 2
+    for i in range(len(betas)-1):
+        alphas[i+1] = (betas[i] + betas[i+1]) / 2
+        
+
     
+    Z = np.empty((M, K))
     # Draw samples from the alphas to construct Z
     
     for i in range(M):
         for j in range(K):
-            Z[i,j] = np.random.choice(alphas, size=1)
+            idx = np.random.randint(0, len(alphas))
+            # Z[i,j] = np.random.choice(alphas, size=1)
+            Z[i,j] = alphas[idx]
     
     
-    # "non-random Z"
+    # # "non-random Z"
     # cutoff_vals = [int(np.round( ((i+1) * len(alphas) - 1) / 4)) for i in range(4)]
     # # print(cutoff_vals)
     # # print(cutoff_vals)
     # # print(alphas)
-    # p_deviation = 0
+    # p_deviation = 0.4
     # for i in range(M):
     #     for j in range(K):
     #         if not np.random.choice([0,1], p=(1-p_deviation, p_deviation)) == 1:
@@ -67,14 +70,14 @@ def get_Z(M, K, p):
     #         else:
     #             Z[i,j] = np.random.choice(alphas, size=1)
     # print("n.o. alphas: ", len(alphas))
+    
     return Z, betas
 
 def get_A(N, K):
-    np.random.seed(42) # set another seed :)
+    np.random.seed(123) # set another seed :)
     
-    alpha = np.array([1]*K)
-    
-    return np.random.dirichlet(alpha, size=N).transpose()
+    a = np.array([1]*K)
+    return np.random.dirichlet(a, size=N).transpose()
 
 
 def get_D(X, betas, sigma):
@@ -92,7 +95,8 @@ def get_D(X, betas, sigma):
         elif j == J+1:
             D[j] = np.ones((M,N))*(np.inf)
         else:
-            D[j] = (betas[j-1] - X)/(softPlus(sigma)) ## Add softplus(sigma)
+            D[j] = (betas[j-1] - X)/(softPlus(sigma)) # Softplus = np.log(1+np.exp(sigma))
+
     return D
 
 def Probs(D):
@@ -100,10 +104,17 @@ def Probs(D):
     J, M, N = D.shape
     
     probs = np.empty((J-1, M, N)) 
-    for i in range(J):
-        if i != J-1:
-            probs[i,:,:] = norm.cdf(D[i+1], loc=0, scale=1) - norm.cdf(D[i], loc=0, scale=1)
-            
+    
+    # D_cdf = stand_norm.cdf(D)
+    # P = D_cdf[1:]-D_cdf[:len(D)-1]
+    
+    probs = norm.cdf(D[1:], loc=0, scale=1)-norm.cdf(D[:len(D)-1], loc=0, scale=1)
+    
+    # for i in range(J):
+    #     if i != J-1:
+    #         probs[i,:,:] = norm.cdf(D[i+1], loc=0, scale=1) - norm.cdf(D[i], loc=0, scale=1)
+    
+    
     return probs
 
 def toCategorical(probs):
@@ -115,17 +126,17 @@ def toCategorical(probs):
     for m in range(M):
         for n in range(N):
             X_cat[m,n] = np.random.choice(categories, p = list(probs[:,m,n]))
-    
+
     return X_cat
     
     
 #%%
 
-M = 20
-N = 10000
-p = 5
-K = 3
-sigma = 0.001
+M = 15
+N = 1000
+p = 6
+K = 5
+sigma = 1
 
 
 Z, betas = get_Z(M,K,p)
@@ -135,6 +146,33 @@ D = get_D(X_raw, betas, sigma)
 
 probs = Probs(D)
 X_cat = toCategorical(probs)
+
+J, _,_ = D.shape
+
+# print("betas", betas)
+# # print("rå data", X_raw)
+# print("mean:", np.mean(X_raw))
+# print("median:", np.median(X_raw))
+
+print("betas: ", betas)
+
+
+# for i in range(J):
+#     print(D[i,0,0])
+# print("probs:", (probs[0,0,0]))
+# for i in range(J):
+#     print(D[i,1,1])
+# print("probs:", (probs[0,1,1]))
+# for i in range(J):
+#     print(D[i,2,2])
+# print("probs:", (probs[0,2,2]))
+# for i in range(J):
+#     print(D[i,3,3])
+
+# print(probs[0,3,3])
+
+
+
 
 #%%
 print("betas", betas)
@@ -174,4 +212,5 @@ for i in range(len(probs)):
     s_probs += probs[i]
 print(s_probs)
 
+#%%
 
