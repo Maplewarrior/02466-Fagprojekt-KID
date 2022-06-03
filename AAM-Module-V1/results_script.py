e6a2bdc8f1ff70e5a4b90a831189f4963c455ed2
FenrisWulven <ollie.elmgreen1@gmail.com>
2022-06-03 11:00:43 +0200

    deleted AAM-V2 because of confusion and uselessness

--- AAM-Module-V1/results_script.py
+++ AAM-Module-V1/results_script.py
@@ -5,7 +5,7 @@ from AAM import AA
 """
 ###### Setting the variables ######
       
-     N: Number of respondents
+     N: Number of respodents
      M: Number of questions
      K: Number of archetypes
      p: Length of Likert scale
@@ -21,7 +21,7 @@ from AAM import AA
 """
 N = 10000
 M = 10
-K = 4
+K = 3
 p = 5
 
 sigma = 1
@@ -37,7 +37,7 @@ AAM = AA()
 AA_types = ["CAA", "OAA", "ROBAA", "TSAA"]
 archetypes = [3, 5, 7]
 
-
+AA_types1 = ["CAA","TSAA"]
 # sigma_vals = [-1000000, -10, 0, 0.5, 1, 2, 10, 100, 1000, 10000]
 sigma_vals = [-1000, 1, 10]
 
@@ -45,10 +45,15 @@ b_param_vals = []
 a_param_vals = []
 
 
-for idx, s in enumerate(sigma_vals):
-    AAM.create_synthetic_data(N=N, M=M, K=K, p=p, sigma=s)
+
+counter1 = 0
+for type in AA_types1:
+    for s in sigma_vals:
+        AAM.create_synthetic_data(N=N, M=M, K=K, p=p, sigma=s)
     
-    for idx2, k in enumerate(archetypes):
-        AAM.analyse(AA_type = "CAA", with_synthetic_data = True, K=k, n_iter = n_iter)
-        AAM.save_analysis(filename = "Analysis"+ "_sigma_" + str(s) + "_Arche_" + str(k), result_number = idx2, with_synthetic_data=True) 
+        for k in archetypes:
+            AAM.analyse(AA_type = type, with_synthetic_data = True, K=k, n_iter = n_iter)
+            AAM.save_analysis(filename =  "_sigma_" + str(s) + "_Arche_" + str(k), model_type = type, result_number = 0, with_synthetic_data=True) 
+        
+            
         
