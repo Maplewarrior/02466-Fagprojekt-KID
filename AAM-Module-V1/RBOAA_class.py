a3390c616d26c84afe05ce01a4f3d1d5e68cf1a9
FenrisWulven <ollie.elmgreen1@gmail.com>
2022-06-06 23:25:25 +0200

    fixed RBOAA loss plot

--- AAM-Module-V1/RBOAA_class.py
+++ AAM-Module-V1/RBOAA_class.py
@@ -82,7 +82,6 @@ class _RBOAA:
         M_arange = [m for m in range(M) for n in range(N)]
 
         loss = torch.sum(inverse_log_P[torch.flatten(X.long())-1,N_arange,M_arange])
-
         return loss
 
     def _error(self,Xt,A_non_constraint,B_non_constraint,b_non_constraint,sigma_non_constraint,J):
@@ -126,7 +125,7 @@ class _RBOAA:
                 loading_bar._update()
             optimizer.zero_grad()
             L = self._error(Xt,A_non_constraint,B_non_constraint,b_non_constraint,sigma_non_constraint,J)
-            self.loss.append(L)
+            self.loss.append(L.detach().numpy())
             L.backward()
             optimizer.step() 
         
