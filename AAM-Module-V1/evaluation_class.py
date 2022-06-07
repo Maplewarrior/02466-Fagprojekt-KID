c7f53a544f34e1b44cf6bd7f078799da15a8c6fe
anden1305 <andreasbigom@gmail.com>
2022-06-07 22:58:43 +0200

    new data and new evaluation script with data loading

--- AAM-Module-V1/evaluation_class.py
+++ AAM-Module-V1/evaluation_class.py
@@ -1,8 +1,46 @@
 ########## IMPORT ##########
 import numpy as np
+import os
+import pickle
 
 ########## EVALUATION CLASS ##########
 class _evaluation:
+
+    def load_results(self):
+        results = {}
+        directory = 'synthetic_results'
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                file = open(filepath,'rb')
+                result = pickle.load(file)
+
+                AA_type = filename.split("_")[0]
+                sigma = float(filename.split("_")[2])
+                k = int(filename.split("_")[4])
+                a = float(filename.split("_")[6])
+                b = float(filename.split("_")[8])
+                
+                if not AA_type in results:
+                    results[AA_type] = {}
+                if not sigma in results[AA_type]:
+                    results[AA_type][sigma] = {}
+                if not k in results[AA_type][sigma]:
+                    results[AA_type][sigma][k] = {}
+                if not a in results[AA_type][sigma][k]:
+                    results[AA_type][sigma][k][a] = {}
+                if not b in results[AA_type][sigma][k][a]:
+                    results[AA_type][sigma][k][a][b] = {}
+                if "metadata" in filename:
+                    if not "metadata" in results[AA_type][sigma][k][a][b]:
+                        results[AA_type][sigma][k][a][b]["metadata"] = []
+                    results[AA_type][sigma][k][a][b]["metadata"].append(result)
+                elif not "metadata" in filename:
+                    if not "analysis" in results[AA_type][sigma][k][a][b]:
+                        results[AA_type][sigma][k][a][b]["analysis"] = []
+                    results[AA_type][sigma][k][a][b]["analysis"].append(result)
+
+        return results
 
     def _matrix_correlation_coefficient(self, A1, A2):
         K, _ = A1.shape() #kolonner - btw A1 og A2 skal have samme antal kolonner aka archetyper
@@ -39,3 +77,8 @@ class _evaluation:
         for i in range(k):
             total += abs(b1[i]-b2[i])
         return total/k
+
+
+ec = _evaluation()
+results = ec.load_results()
+print(results)
--- AAM-Module-V1/load_results.py
+++ AAM-Module-V1/load_results.py
@@ -1,10 +1,8 @@
 import os
 import pickle
-from collections import defaultdict
 
 
 results = {}
-
 directory = 'synthetic_results'
 for filename in os.listdir(directory):
     filepath = os.path.join(directory, filename)
@@ -13,8 +11,10 @@ for filename in os.listdir(directory):
         result = pickle.load(file)
 
         AA_type = filename.split("_")[0]
-        sigma = float(filename.split("_")[3])
-        k = int(filename.split("_")[5].replace(".obj",""))
+        sigma = float(filename.split("_")[2])
+        k = int(filename.split("_")[4])
+        a = float(filename.split("_")[6])
+        b = float(filename.split("_")[8])
         
         if not AA_type in results:
             results[AA_type] = {}
@@ -22,12 +22,19 @@ for filename in os.listdir(directory):
             results[AA_type][sigma] = {}
         if not k in results[AA_type][sigma]:
             results[AA_type][sigma][k] = {}
+        if not a in results[AA_type][sigma][k]:
+            results[AA_type][sigma][k][a] = {}
+        if not b in results[AA_type][sigma][k][a]:
+            results[AA_type][sigma][k][a][b] = {}
         if "metadata" in filename:
-            if not "metadata" in results[AA_type][sigma][k]:
-                results[AA_type][sigma][k]["metadata"] = []
-            results[AA_type][sigma][k]["metadata"].append(result)
+            if not "metadata" in results[AA_type][sigma][k][a][b]:
+                results[AA_type][sigma][k][a][b]["metadata"] = []
+            results[AA_type][sigma][k][a][b]["metadata"].append(result)
         elif not "metadata" in filename:
-            if not "analysis" in results[AA_type][sigma][k]:
-                results[AA_type][sigma][k]["analysis"] = []
-            results[AA_type][sigma][k]["analysis"].append(result)
+            if not "analysis" in results[AA_type][sigma][k][a][b]:
+                results[AA_type][sigma][k][a][b]["analysis"] = []
+            results[AA_type][sigma][k][a][b]["analysis"].append(result)
+        
+        print(AA_type + "_" + str(sigma) + "_" + str(k) + "_" + str(a) + "_" + str(b))
 
+print(results["CAA"][-0.43][3][0.85][1.0])
