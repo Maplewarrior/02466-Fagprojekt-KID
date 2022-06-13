ae64e1b5702f0f2e6f0d490b0ec2bc89b35927b6
anden1305 <andreasbigom@gmail.com>
2022-06-13 17:27:31 +0200

    FIX OF BDM

--- AAM-Module-V1/test_env2.py
+++ AAM-Module-V1/test_env2.py
@@ -13,12 +13,12 @@ from RBOAA_class import _RBOAA
 
 
 AAM = AA()
-AAM.create_synthetic_data(10000,21,5,6,-2.25,True,10,1,True,sigma_std=0.5)
+AAM.create_synthetic_data(1000,21,5,6,-2.25,False,10,1,True,sigma_std=0.5)
 
 # plotter = _plots()
 # plotter._barplot_all(AAM._synthetic_data.Z,AAM._synthetic_data.columns)
 
-AAM.analyse(5,6,10000,True,"RBOAA",0.025,False,True,True)
+AAM.analyse(5,6,10000,True,"OAA",0.025,False,True,True)
 
 # AAM.plot("RBOAA","loss_plot",with_synthetic_data=True)
 # AAM.plot("RBOAA","barplot_all",with_synthetic_data=True)
@@ -27,6 +27,6 @@ AAM.analyse(5,6,10000,True,"RBOAA",0.025,False,True,True)
 # print(AAM._synthetic_data.betas)
 # print(AAM._synthetic_results["RBOAA"][0].b)
 
-print(MCC(AAM._synthetic_results["RBOAA"][0].Z,AAM._synthetic_data.Z))
-print(NMI(AAM._synthetic_results["RBOAA"][0].A,AAM._synthetic_data.A))
-print(BDM(AAM._synthetic_data.betas,AAM._synthetic_results["RBOAA"][0].b, "RBOAA"))
+# print(MCC(AAM._synthetic_results["RBOAA"][0].Z,AAM._synthetic_data.Z))
+# print(NMI(AAM._synthetic_results["RBOAA"][0].A,AAM._synthetic_data.A))
+print(BDM(AAM._synthetic_data.betas,AAM._synthetic_results["OAA"][0].b, "OAA"))
