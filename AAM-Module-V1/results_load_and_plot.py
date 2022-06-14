46089e931ddc91b0092424aa423ad08354baa3fb
HarbX <michaelharborg@gmail.com>
2022-06-14 19:22:23 +0200

    done

--- AAM-Module-V1/results_load_and_plot.py
+++ AAM-Module-V1/results_load_and_plot.py
@@ -13,6 +13,8 @@ attributeNames = list(df.columns)
 # Sorting the values in the df
 df = df.sort_values(["sigma", "a_param", "b_param", "synthetic_k", "sigma_std", "rep"], ascending = [True, True, True, True, True, True])
 
+
+
 # Find the number of unique parameter values
 sigmas = list(df["sigma"].unique())
 a_params = list(df["a_param"].unique())
@@ -24,7 +26,7 @@ sigma_stds = list(df["sigma_std"].unique())
 AA_types = list(df["AA_type"].unique())
 metrics = ['loss', 'NMI', 'MCC', 'BDM', 'Est. sigma']
 
-
+#%%
 data_dict = {}
 
 def isNaN(num):
@@ -67,22 +69,25 @@ for a_param in a_params:
                                         if not sigma in data_dict[a_param][b_param][k][s_std][type][k_anal][metric]:
                                             data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma] = []
                                         
-                                        for rep in range(n_reps):
-                                            
-                                            mask = df.loc[(df['a_param'] == a_param) & (df['b_param'] == b_param) 
-                                                   & (df['synthetic_k'] == k) & (df['sigma_std'] == s_std) 
-                                                   & (df['sigma'] == sigma) & (df['rep'] == rep) & (df["AA_type"] == type)]
-                                            
-                                            metric_dict[metric].append(mask[metric])
                                         
+                                            
+                                        mask = df.loc[(df['a_param'] == a_param) & (df['b_param'] == b_param) 
+                                               & (df['synthetic_k'] == k) & (df['sigma_std'] == s_std) 
+                                               & (df['sigma'] == sigma) & (df["AA_type"] == type)]
+                                            
+                                        metric_dict[metric] = mask[metric]
                                         ### Add mean values
-                                        if not isNaN(metric_dict[metric]):
+                                        if not isNaN(metric_dict[metric].any()):
                                             
-                                            data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma].append(np.mean(metric_dict[metric]))
-                                        
+                                            if metric in ["NMI", "MCC"]:
+                                                data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma].append((metric_dict[metric]))
+                                            elif metric in ["BDM", "loss"]:
+                                                data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma].append((metric_dict[metric]))
+                                            else:
+                                                data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma].append((metric_dict[metric]))
                                         else:
                                             data_dict[a_param][b_param][k][s_std][type][k_anal][metric][sigma].append("NaN")
-                                    
+print("Created data dictionary succesfully")                              
 #%% 
 # print(df['a_param'][0])
 # print(df['b_param'][0])
@@ -94,7 +99,11 @@ for a_param in a_params:
 # print(df['rep'][0])
 # print(df["BDM"][110])
 
-# print(data_dict[2.0]["RB_false"][5.0][0.0]["CAA"][5.0]["NMI"][-20.0])
+m = df.loc[(df['a_param'] == 1) & (df['b_param'] == 'RB_false') 
+             & (df['synthetic_k'] == 5) & (df['sigma_std'] == 1.0) 
+              & (df["AA_type"] == "RBOAA") & (df["sigma"]==-20.0)]
+print(m["NMI"])
+
 #%%
 ######## MAKE PLOTS ######### 
 # a, b, syn_k, s_std, type, anal_k, metrics, sigmas
@@ -108,8 +117,10 @@ sigmas_softplussed = [softplus(s) for s in sigmas]
 
 # print(flatten(list(data_dict[a_params[0]][b_params[0]][synthetic_ks[0]][sigma_stds[0]][AA_types[0]][analysis_ks[0]][metrics[0]].values())))
 
-# print(data_dict[a_params[0]][b_params[0]][synthetic_ks[0]])
+# print(data_dict[a_params[0]][b_params[0]][synthetic_ks[0]][sigma_stds[0]]["CAA"][5]["NMI"][-20.0])
 
+print("max val:", np.max(data_dict[a_params[0]][b_params[0]][synthetic_ks[0]][sigma_stds[0]]["CAA"][5]["NMI"][-20.0]))
+#%%
 
 path = os.getcwd()
 foldername = "result_plots_final"
@@ -120,20 +131,51 @@ for a_param in a_params:
         for k_syn in synthetic_ks:
             for s_std in sigma_stds:
                 for k_anal in analysis_ks:
-                    # for sigma in sigmas:
+                    NMI_OAA, MCC_OAA, loss_OAA, sigma_est_OAA, BDM_OAA = [], [], [], [], []
+                    NMI_RBOAA, MCC_RBOAA, loss_RBOAA, sigma_est_RBOAA, BDM_RBOAA = [], [], [], [], []
+                    
+                    NMI_CAA, MCC_CAA, loss_CAA = [], [], []
+                    NMI_TSAA, MCC_TSAA, loss_TSAA = [], [], []
+
+                    for sigma in sigmas:
                         
-                   
+                        # Calc values for OAA
+                        NMI_OAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["NMI"][sigma]))))
+                        MCC_OAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["MCC"][sigma]))))
+                        loss_OAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["loss"][sigma]))))
+                        BDM_OAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["BDM"][sigma]))))
+                        sigma_est_OAA.append(np.mean(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["Est. sigma"][sigma]))))
+                        
+                         # Calc values for RBOAA
+                        NMI_RBOAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["NMI"][sigma]))))
+                        MCC_RBOAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["MCC"][sigma]))))
+                        loss_RBOAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["loss"][sigma]))))
+                        BDM_RBOAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["BDM"][sigma]))))
+                        sigma_est_RBOAA.append(np.mean(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["Est. sigma"][sigma]))))
+                        
+                        # Calc values for CAA
+                        NMI_CAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["NMI"][sigma]))))
+                        MCC_CAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["MCC"][sigma]))))
+                        loss_CAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["loss"][sigma]))))
+                        
+                        # Calc values for TSAA
+                        NMI_TSAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["NMI"][sigma]))))
+                        MCC_TSAA.append(np.max(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["MCC"][sigma]))))
+                        loss_TSAA.append(np.min(flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["loss"][sigma]))))
+                        
+                        
+
                     
                     # Make combined NMI and MCC plots for OAA and RBOAA
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["NMI"].values())), label = "OAA " + "NMI")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["NMI"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["NMI"].values())), label = "RBOAA " + "NMI")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["NMI"].values())), '--')
+                    plt.scatter(sigmas_softplussed, NMI_OAA, label = "OAA " + "NMI")
+                    plt.plot(sigmas_softplussed, NMI_OAA, '--')
+                    plt.scatter(sigmas_softplussed, NMI_RBOAA, label = "RBOAA " + "NMI")
+                    plt.plot(sigmas_softplussed, NMI_RBOAA, '--')
                     
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["MCC"].values())), label = "OAA " + "MCC")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["MCC"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["MCC"].values())), label = "RBOAA " + "MCC")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["MCC"].values())), '--')
+                    plt.scatter(sigmas_softplussed, MCC_OAA, label = "OAA " + "MCC")
+                    plt.plot(sigmas_softplussed, MCC_OAA, '--')
+                    plt.scatter(sigmas_softplussed, MCC_RBOAA, label = "RBOAA " + "MCC")
+                    plt.plot(sigmas_softplussed, MCC_RBOAA, '--')                    
                     
                     plt.xlabel('sigma', fontsize=14)
                     plt.ylabel('NMI and MCC', fontsize=14)
@@ -141,21 +183,19 @@ for a_param in a_params:
                     plt.legend()
                     file = ("OAA+RBOAA NMI & MCC,a={0}, b={1}, k_syn={2}, k_anal={3}, s_std={4}.png".format(a_param, b_param, k_syn, k_anal, s_std))
                    
-                   
                     plt.savefig(os.path.join(path,file))
                     plt.close()
 
                     # Make combined NMI and MCC plots for CAA and TSAA
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["NMI"].values())), label = "CAA " + "NMI")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["NMI"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["NMI"].values())), label = "TSAA " + "NMI")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["NMI"].values())), '--')
+                    plt.scatter(sigmas_softplussed, NMI_CAA, label = "CAA " + "NMI")
+                    plt.plot(sigmas_softplussed, NMI_CAA, '--')
+                    plt.scatter(sigmas_softplussed, NMI_TSAA, label = "TSAA " + "NMI")
+                    plt.plot(sigmas_softplussed, NMI_TSAA, '--')
                     
-                    
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["MCC"].values())), label = "CAA " + "MCC")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["MCC"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["MCC"].values())), label = "TSAA " + "MCC")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["MCC"].values())), '--')
+                    plt.scatter(sigmas_softplussed, MCC_CAA, label = "CAA" + "MCC")
+                    plt.plot(sigmas_softplussed, MCC_CAA, '--')
+                    plt.scatter(sigmas_softplussed, MCC_TSAA, label = "TSAA " + "MCC")
+                    plt.plot(sigmas_softplussed, MCC_TSAA, '--')                
                     
                     plt.xlabel('sigma', fontsize=14)
                     plt.ylabel('NMI and MCC', fontsize=14)
@@ -166,10 +206,10 @@ for a_param in a_params:
                     plt.close()                        
 
                     # Make loss plots for RBOAA, OAA
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["loss"].values())), label = "OAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["loss"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["loss"].values())), label = "RBOAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["loss"].values())), '--')
+                    plt.scatter(sigmas_softplussed, loss_OAA, label = "OAA ")
+                    plt.plot(sigmas_softplussed, loss_OAA, '--')
+                    plt.scatter(sigmas_softplussed, loss_RBOAA, label = "RBOAA ")
+                    plt.plot(sigmas_softplussed, loss_RBOAA, '--')
 
                     plt.xlabel('sigma', fontsize=13)
                     plt.ylabel('loss', fontsize=13)
@@ -184,10 +224,10 @@ for a_param in a_params:
                     
                     
                     # Make loss plots for CAA and TSAA
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["loss"].values())), label = "CAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["CAA"][k_anal]["loss"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["loss"].values())), label = "TSAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["TSAA"][k_anal]["loss"].values())), '--')
+                    plt.scatter(sigmas_softplussed, loss_CAA, label = "CAA ")
+                    plt.plot(sigmas_softplussed, loss_CAA, '--')
+                    plt.scatter(sigmas_softplussed, loss_TSAA, label = "TSAA ")
+                    plt.plot(sigmas_softplussed, loss_TSAA, '--')
 
                     plt.xlabel('sigma', fontsize=14)
                     plt.ylabel('loss', fontsize=14)
@@ -200,10 +240,10 @@ for a_param in a_params:
                     
                         
                     ## Make sigma plot
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["Est. sigma"].values())), label = "OAA" )
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["Est. sigma"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["Est. sigma"].values())), label = "RBOAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["Est. sigma"].values())), '--')
+                    plt.scatter(sigmas_softplussed, sigma_est_OAA, label = "OAA" )
+                    plt.plot(sigmas_softplussed, sigma_est_OAA, '--')
+                    plt.scatter(sigmas_softplussed, sigma_est_RBOAA, label = "RBOAA ")
+                    plt.plot(sigmas_softplussed, sigma_est_RBOAA, '--')
                     
                     plt.xlabel('true sigma', fontsize=14)
                     plt.ylabel('est. sigma', fontsize=14)
@@ -215,10 +255,10 @@ for a_param in a_params:
                     
                     
                     # Make BDM plot
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["BDM"].values())), label = "OAA" )
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["OAA"][k_anal]["BDM"].values())), '--')
-                    plt.scatter(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["BDM"].values())), label = "RBOAA ")
-                    plt.plot(sigmas_softplussed, flatten(list(data_dict[a_param][b_param][k_syn][s_std]["RBOAA"][k_anal]["BDM"].values())), '--')
+                    plt.scatter(sigmas_softplussed, BDM_OAA, label = "OAA" )
+                    plt.plot(sigmas_softplussed, BDM_OAA, '--')
+                    plt.scatter(sigmas_softplussed, BDM_RBOAA, label = "RBOAA ")
+                    plt.plot(sigmas_softplussed, BDM_RBOAA, '--')
                     
                     plt.xlabel('sigma', fontsize=14)
                     plt.ylabel('BDM', fontsize=14)
@@ -231,15 +271,9 @@ for a_param in a_params:
                     
                     
                     
-                    
-                        
-                        
-                        
-                    
-                    
-        
+print("plots created and saved in folder {}".format(path))
 #%%
-print(data_dict[0.5]['1'][5.0][0]["RBOAA"][5.0]["BDM"][-20.0])
+# print(data_dict[0.5]['1'][5.0][0]["RBOAA"][5.0]["BDM"][-20.0])
 
                             
                                 
