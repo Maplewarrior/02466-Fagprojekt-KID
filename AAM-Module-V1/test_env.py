0700dc80da97a3c439b0c576ac4ac6441927bde8
anden1305 <andreasbigom@gmail.com>
2022-06-19 14:35:10 +0200

    stability of models over varying k push

--- AAM-Module-V1/test_env.py
+++ AAM-Module-V1/test_env.py
@@ -13,7 +13,7 @@ from TSAA_class import _TSAA
 
 AAM = AA()
 
-AAM.create_synthetic_data(10000,21,5,6,-20,False,1,1,False,0)
+AAM.create_synthetic_data(1000,21,5,6,-20,False,1,1,False,0)
 
 AAM.analyse(10,6,2000,False,"RBOAA",0.01,False,True,True)
 print(AAM._synthetic_results["RBOAA"][0].loss)
